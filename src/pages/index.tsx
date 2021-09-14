e61f097f88145d9347bcc55d722448b26978a1e1
Nelson <nelsonsozinho@gmail.com>
2021-09-14 19:47:49 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,81 @@
+import { GetStaticProps } from 'next';
+import Header from '../components/Header';
+import {FiCalendar, FiUser} from 'react-icons/fi';
+import Link from 'next/link';
+
+import commonStyles from '../styles/common.module.scss';
+import styles from './home.module.scss';
+
+interface Post {
+  uid?: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    author: string;
+  };
+}
+
+interface PostPagination {
+  next_page: string;
+  results: Post[];
+}
+
+interface HomeProps {
+  postsPagination: PostPagination;
+}
+
+export default function Home() {
+  return(
+    <>
+      <main className={commonStyles.container}>
+        <Header/>   
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Novo titulo</strong>
+              <p>Teste de texto</p>
+              <ul>
+                <li>
+                  <FiCalendar/>
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser/>
+                  Nelson
+                </li>
+              </ul>
+            </a>  
+          </Link>
+
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum Titulo</strong>
+              <p>Teste de texto</p>
+              <ul>
+                <li>
+                  <FiCalendar/>
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser/>
+                  Nelson
+                </li>
+              </ul>
+            </a>  
+          </Link>
+          <button type="button">
+            Carregar mais posts
+          </button>
+        </div>     
+      </main>
+    </>
+  )
+}
+
+// export const getStaticProps = async () => {
+//   // const prismic = getPrismicClient();
+//   // const postsResponse = await prismic.query(TODO);
+
+//   // TODO
+// };
